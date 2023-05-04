bf4dfc3a310b91fa0137a524605a345f4f31b745
Cheesehyvel <eric.hoglander@gmail.com>
2023-05-04 15:00:58 +0200

    Added link to sim in description (Fixed #28). Updated p3 kirin tor ring.

--- assets/js/items.js
+++ assets/js/items.js
@@ -1208,7 +1208,7 @@ var equip = {
         { id: 47618, title: "Firestorm Ring", ilvl: 232, int: 52, sp: 78, crit: 36, hit: 50, unique: true, faction: "a", phase: 3 },
         { id: 47732, title: "Band of the Invoker", ilvl: 245, int: 58, spi: 50, sp: 89, haste: 50, unique: true, phase: 3 },
         { id: 47733, title: "Heartmender Circle", ilvl: 245, int: 58, sp: 89, crit: 50, mp5: 25, unique: true, phase: 3 },
-        { id: 48957, title: "Etched Signet of the Kirin Tor", ilvl: 226, int: 43, sp: 74, crit: 41, haste: 43, unique: true, phase: 3 },
+        { id: 48957, title: "Etched Signet of the Kirin Tor", ilvl: 232, int: 45, sp: 78, crit: 43, haste: 45, unique: true, phase: 3 },
         // ulduar
         { id: 45495, title: "Conductive Seal", ilvl: 252, int: 54, spi: 44, sp: 85, haste: 62, sockets: ["b"], bonus: {sp: 5}, unique: true, phase: 2 },
         { id: 45946, title: "Fire Orchid Signet", ilvl: 239, int: 56, sp: 84, haste: 34, mp5: 22, sockets: ["y"], unique: true, phase: 2 },
--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 Mage DPS simulator for WoW Wotlk Classic.
 
+[Live sim website](https://cheesehyvel.github.io/magesim-wotlk/)
+
 Backend is written in C++ and compiled with Emscripten into WebAssembly.\
 Frontend is written in Vue.js and SCSS and compiled with Laravel mix.
 
